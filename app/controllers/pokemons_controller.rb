b3cd44d26e2453f55dbbbd18c204bf68ee4b3eed
Chris Boertien <chris.boertien@gmail.com>
2016-08-22 01:13:49 -0300

    lookup a pokeauth before fetching pokemons for the user
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,15 +2,22 @@ class PokemonsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @pokemons = Pokemon.all
-    if @pokemons.any?
-      render :index
-    else
-      render :no_pokemon
-    end
+    @poke_auth = load_poke_auth
+    render :no_pokemon and return if @poke_auth.nil? || @poke_auth.pokemons.empty?
+    @pokemons = @poke_auth.pokemons
   end
 
   def csv
     @pokemons = Pokemon.all
+  end
+
+private
+
+  def load_poke_auth
+    if current_user.has_multiple_accounts? and not params[:poke_auth_id].nil?
+      PokeAuth.find(params[:poke_auth_id])
+    else
+      current_user.poke_auths.default
+    end
   end
 end
